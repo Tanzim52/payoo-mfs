4f48520dbc0bf8d541d283cedb561502711fe944
Tanzim52 <mahin1575@gmail.com>
2024-10-13 20:30:09 +0600

    first commit

---
+++ login.js
@@ -0,0 +1,18 @@
+
+
+
+document.getElementById('login-btn').addEventListener('click', function (event) {
+    event.preventDefault();
+    let number = document.getElementById('number').value;
+    let pin = document.getElementById('pin').value;
+    let ogNumber =01744842814;
+    let ogPin = 1111;
+    if (number == ogNumber && pin == ogPin) {
+        window.location.href='/homepage.html'
+
+    }
+    else{
+        alert('Wrong num or pin')
+    }
+
+})
